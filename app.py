729d6c316d9aee58fe154113968a5c7d856e8454
moeom <pippi1go.111113@icloud.com>
2021-07-29 19:42:30 +0900

    add answer page

--- app.py
+++ app.py
@@ -8,14 +8,31 @@ def index():
     return render_template('index.html')
 
 @app.route("/metacrinus", methods=["GET"])
-def get_metacrinus():
+def metacrinus():
     return render_template('layout_quiz.html',
                            title="ウミユリクイズ",
                            message="ウミユリっていつから生きているの？",
                            picture="/static/images/1_umiyurikun_siruetto.png",
                            choice1="約500,000,000～250,000,000年前から",
+                           answer1="location.href='./metacrinus_correct'",
                            choice2="約250,000,000～150,000,000年前から",
-                           choice3="約150,000,000年前から")
+                           answer2="location.href='./metacrinus_incorrect'",
+                           choice3="約150,000,000年前から",
+                           answer3="location.href='./metacrinus_incorrect'")
+
+@app.route("/metacrinus_correct",methods=["GET"])
+def metacrinus_correct():
+    return render_template('correct.html',
+                           title="ウミユリクイズ",
+                           message="ウミユリは約500,000,000年前(オルドビス紀)に出現したとされています。当時の海には他にまっすぐ伸びた殻が特徴的なチョッカクガイや、顎や胸びれの未発達な魚、アランダスピスなど生息していており、無脊椎動物と脊椎動物がともに多様化していった時期として知られています。",
+                           picture="/static/images/1_umiyurikun.png")
+
+
+@app.route("/metacrinus_incorrect",methods=["GET"])
+def metacrinus_incorrect():
+    return render_template('incorrect.html',
+                           title="ウミユリクイズ",
+                           quiz="location.href='./metacrinus'")
 
 #############クイズテンプレート##############
 
