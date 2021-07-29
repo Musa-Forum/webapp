545ad1dcedbe8c0747083f82cdf95fef4ad40c52
moeom <pippi1go.111113@icloud.com>
2021-07-29 19:13:59 +0900

    first commit

--- app.py
+++ app.py
@@ -9,7 +9,27 @@ def index():
 
 @app.route("/metacrinus", methods=["GET"])
 def get_metacrinus():
-    return render_template('metacrinus.html')
+    return render_template('layout_quiz.html',
+                           title="ウミユリクイズ",
+                           message="ウミユリっていつから生きているの？",
+                           picture="/static/images/1_umiyurikun_siruetto.png",
+                           choice1="約500,000,000～250,000,000年前から",
+                           choice2="約250,000,000～150,000,000年前から",
+                           choice3="約150,000,000年前から")
+
+#############クイズテンプレート##############
+
+#@app.route("/name", methods=["GET"])
+#def get_name():
+#    return render_template('layout_quiz.html',
+#                           title="",
+#                           message="",
+#                           picture="",
+#                           choice1="",
+#                           choice2="",
+#                           choice3="")
+
+###########################################
 
 @app.route("/crystal", methods=["GET"])
 def get_crystal():
