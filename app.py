daedfb01186667a1446606da21a26ac6efb832ac
moeom <pippi1go.111113@icloud.com>
2022-02-23 19:55:31 +0900

    edit app 3

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ async def read_item(emailad:str, row:str):
     authorization.permission(emailad,row)
     return{"ping":"pong"}
 
-if __name__ == "__main__":
+def main():
     app.debug = True
+    uvicorn.run(app)
+
+if __name__ == "__main__":
+    main()
+    #app.debug = True
     #app.run(host='127.0.0.1', port=8080)
     #uvicorn.run(app)
-    uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}
